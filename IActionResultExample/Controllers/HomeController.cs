c790b1b42a45d38d95172c2a8534a54e07601963
Liang Wu <liangwuweb@gmail.com>
2023-05-04 15:44:44 -0500

    fix IActionResult

--- IActionResultExample/Controllers/HomeController.cs
+++ IActionResultExample/Controllers/HomeController.cs
@@ -4,40 +4,49 @@ namespace IActionResultExample.Controllers
 {
     public class HomeController : Controller
     {
-        [Route("/book")]
-        public VirtualFileResult Index()
-        {   
-            ////Book id should be appield
-            //if (!Request.Query.ContainsKey("book-id"))
-            //{
-            //    Response.StatusCode = 400;
-            //    return Content("Book id is not applied");
-            //}
+        //[Route("/")]
+        //public ContentResult Index()
+        //{
 
-            ////Book id cannot be empty
-            //if (string.IsNullOrEmpty(Request.Query["book-id"])) {
-            //    Response.StatusCode = 400;
-            //    return Content("Book id is empty");
-            
-            //}
+        //    return Content("<h3>Hello from Index<h3>", "text / html");
 
-            //int bookId = Convert.ToInt16(Request.Query["book-id"]);
-            //if (bookId <= 0 || bookId > 1000) {
-            //    Response.StatusCode = 400;
-            //    return Content("bookId is not in the range");
+        //}
 
-            //}
+        [Route("file-download")]
+        public IActionResult Index()
+        {
+            //Book id should be appield
+            if (!Request.Query.ContainsKey("book-id"))
+            {
+                Response.StatusCode = 400;
+                return Content("Book id is not applied");
+            }
 
-            ////islooedin is true
-            //if (!Convert.ToBoolean(Request.Query["isloggedin"]))
-            //{
-            //    Response.StatusCode = 401;
-            //    return Content("User must be authenticated");
+            //Book id cannot be empty
+            if (string.IsNullOrEmpty(Request.Query["book-id"]))
+            {
+                Response.StatusCode = 400;
+                return Content("Book id is empty");
 
-            //}
+            }
 
-            return File("/sample.pdf", "appliation/pdf");
+            int bookId = Convert.ToInt16(Request.Query["book-id"]);
+            if (bookId <= 0 || bookId > 1000)
+            {
+                Response.StatusCode = 400;
+                return Content("bookId is not in the range");
 
+            }
+
+            //islooedin is true
+            if (!Convert.ToBoolean(Request.Query["isloggedin"]))
+            {
+                Response.StatusCode = 401;
+                return Content("User must be authenticated");
+
+            }
+            //return new VirtualFileResult("/sample.pdf", "application/pdf");
+            return File("/ID.pdf", "application/pdf");
         }
     }
 }
