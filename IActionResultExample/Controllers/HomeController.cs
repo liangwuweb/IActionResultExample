a1b4d394742217342259015c1d6e332fa57bc099
Liang Wu <liangwuweb@gmail.com>
2023-05-04 22:54:39 -0500

    status code result

--- IActionResultExample/Controllers/HomeController.cs
+++ IActionResultExample/Controllers/HomeController.cs
@@ -12,37 +12,34 @@ namespace IActionResultExample.Controllers
 
         //}
 
-        [Route("file-download")]
+        [Route("bookstore")]
         public IActionResult Index()
         {
             //Book id should be appield
             if (!Request.Query.ContainsKey("book-id"))
             {
-                Response.StatusCode = 400;
-                return Content("Book id is not applied");
+         
+                return BadRequest("no book id");
             }
 
             //Book id cannot be empty
             if (string.IsNullOrEmpty(Request.Query["book-id"]))
             {
-                Response.StatusCode = 400;
-                return Content("Book id is empty");
+                return BadRequest("Book id is empty");
 
             }
 
             int bookId = Convert.ToInt16(Request.Query["book-id"]);
             if (bookId <= 0 || bookId > 1000)
             {
-                Response.StatusCode = 400;
-                return Content("bookId is not in the range");
+                return NotFound("bookId is not in the range");
 
             }
 
             //islooedin is true
             if (!Convert.ToBoolean(Request.Query["isloggedin"]))
             {
-                Response.StatusCode = 401;
-                return Content("User must be authenticated");
+                return Unauthorized("User must be authenticated");
 
             }
             //return new VirtualFileResult("/sample.pdf", "application/pdf");
