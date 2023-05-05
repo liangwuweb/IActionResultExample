6f68b4de0a3cbb91bb02688073f4f3a30fadf007
Liang Wu <liangwuweb@gmail.com>
2023-05-05 09:45:07 -0500

    Quey string vs Route data

--- IActionResultExample/Controllers/HomeController.cs
+++ IActionResultExample/Controllers/HomeController.cs
@@ -12,43 +12,30 @@ namespace IActionResultExample.Controllers
 
         //}
 
-        [Route("bookstore")]
-        public IActionResult Index()
+        [Route("bookstore/{bookid?}/{isloggedin?}")]
+        public IActionResult Index(int? bookid, bool? isloggedin)
         {
             //Book id should be appield
-            if (!Request.Query.ContainsKey("book-id"))
+            if (!bookid.HasValue)
             {
          
                 return BadRequest("no book id");
             }
 
-            //Book id cannot be empty
-            if (string.IsNullOrEmpty(Request.Query["book-id"]))
-            {
-                return BadRequest("Book id is empty");
 
-            }
-
-            int bookId = Convert.ToInt16(Request.Query["book-id"]);
-            if (bookId <= 0 || bookId > 1000)
+            if (bookid <= 0 || bookid > 1000)
             {
                 return NotFound("bookId is not in the range");
 
             }
 
             //islooedin is true
-            if (!Convert.ToBoolean(Request.Query["isloggedin"]))
+            if ((bool)!isloggedin)
             {
                 return Unauthorized("User must be authenticated");
 
             }
-            //return new VirtualFileResult("/sample.pdf", "application/pdf");
-            //return new RedirectToActionResult("Books", "Store", new object());//302 found
-            //return RedirectToAction("Books", "Store", new { id = bookId});
-            //return new RedirectToActionResult("Books", "Store", new object(), true); //301 move permantly
-            //return RedirectToActionPermanent("Books", "Store", new { id = bookId });
-            //return LocalRedirectPermanent($"store/book/{bookId}"); // 301
-            return Redirect($"store/books/{bookId}");
+            return Content($"Book id: {bookid}", "text/plain");
         }
     }
 }
